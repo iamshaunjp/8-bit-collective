768ea864d49ad476192d0548cc7cb3ad35e4dd67
iamshaunjp <shaun@thenetninja.co.uk>
2024-11-03 11:09:24 +0000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Modal functionality
+const modal = document.getElementById("modal");
+const openModalButton = document.getElementById("open-modal");
+const closeModalButton = document.getElementById("close-modal");
+
+openModalButton.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Close modal if clicked outside content
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
